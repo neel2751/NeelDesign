9666487271307f4cf8f162081e7df6db248ac7ba
neel2751 <patelneel1732@gmail.com>
2022-08-04 20:04:47 +0530

    Neel Design

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html',
+  './**/*.html',
+  './pages/**/*.html',
+  './js/**/*.js',
+  './faqs.html',
+  '*',
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
