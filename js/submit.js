b3f37ddf43c46f8bb93d9871d137cd4bced635d6
neel2751 <patelneel1732@gmail.com>
2022-08-10 13:20:01 +0530

    new

---
+++ js/submit.js
@@ -0,0 +1,10 @@
+
+const scriptURL =
+                  "https://script.google.com/macros/s/AKfycbywPXtxsW53oQ8xj4ffnKIHXLIdm32yKEs1IwyklrlvKOnQc1qM4bm_rdYe7wMdQh-bvw/exec";
+    const form = document.forms["submit-email"];
+        form.addEventListener("submit", (e) => {
+                  e.preventDefault();
+                  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+                    .then((response) => console.log("Success!", response))
+                    .catch((error) => console.error("Error!", error.message));
+});
